178a5e55e3789384fb14df9f05a77c0f8338a534
Ilgu Lee <iglee2000ca@gmail.com>
2019-02-20 18:54:44 -0500

    Implement MutableLiveData and Observe

--- app/src/main/java/ilgulee/com/livedata/MainActivity.kt
+++ app/src/main/java/ilgulee/com/livedata/MainActivity.kt
@@ -3,6 +3,7 @@ package ilgulee.com.livedata
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,12 +16,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var model = ViewModelProviders.of(this).get(MainActivityDataGenerator::class.java)
+        model.getNumber().observe(this, Observer<String> {
+            text_number.text = it
+            Log.i(TAG, "Random number is set: " + it)
+        })
 
-        val model = ViewModelProviders.of(this).get(MainActivityDataGenerator::class.java)
-        model.getNumber()
-        val myRandomNumber = model.getNumber()
-        text_number.text = myRandomNumber
-        Log.i(TAG, "Random number is set: " + myRandomNumber)
-
+        button_fetch.setOnClickListener {
+            model.createNumber()
+        }
     }
+
 }
--- app/src/main/java/ilgulee/com/livedata/MainActivityDataGenerator.kt
+++ app/src/main/java/ilgulee/com/livedata/MainActivityDataGenerator.kt
@@ -1,28 +1,30 @@
 package ilgulee.com.livedata
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.util.*
 
 class MainActivityDataGenerator : ViewModel() {
-    private lateinit var myRandomNumber: String
+
+    private lateinit var myRandomNumber: MutableLiveData<String>
 
     companion object {
         private val TAG: String? = MainActivityDataGenerator::class.java.simpleName
     }
 
-    fun getNumber(): String {
+    fun getNumber(): MutableLiveData<String> {
         Log.i(TAG, "Get number")
         if (!::myRandomNumber.isInitialized) {
+            myRandomNumber=MutableLiveData()
             this.createNumber()
         }
         return myRandomNumber
     }
 
-    private fun createNumber() {
+    public fun createNumber() {
         Log.i(TAG, "Create new number")
         val random = Random()
-        myRandomNumber = "Number: " + (random.nextInt(10 - 1) + 1)
-
+        myRandomNumber.value = "Number: " + (random.nextInt(10 - 1) + 1)
     }
 }
